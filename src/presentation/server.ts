6539eb4074b6b5dae76b5fe8f91cc4376fc9fd83
Juan Pablo Leon <juanleon8581@gmail.com>
2025-08-22 01:26:08 -0500

    fix(server): update server log message to include full URL
    Include 'http://localhost' prefix in the console log message for better clarity when starting the server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -24,7 +24,7 @@ export class Server {
     this.app.use(this.router);
 
     this.app.listen(this.port, () => {
-      console.log(`Server is running on port ${this.port}`);
+      console.log(`Server is running on port http://localhost:${this.port}`);
     });
   }
 }
