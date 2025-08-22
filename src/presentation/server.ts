3c50b9d5611b25c4c3701f9095bd3a8d759243dc
Juan Pablo Leon <juanleon8581@gmail.com>
2025-08-22 00:52:09 -0500

    refactor: extract server logic into separate class
    Move express server setup and configuration to new Server class to improve code organization and maintainability

---
+++ src/presentation/server.ts
@@ -0,0 +1,30 @@
+import express, { Router } from "express";
+
+interface IOptions {
+  port: number;
+  routes: Router;
+}
+
+export class Server {
+  private readonly app = express();
+  private readonly port: number;
+  private readonly router: Router;
+
+  constructor(options: IOptions) {
+    this.port = options.port;
+    this.router = options.routes;
+  }
+
+  async start() {
+    //* Middlewares
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+
+    //* Routes
+    this.app.use(this.router);
+
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
+}
